31cf7940c4a3cc38b2f1ae88818aec614a17f44b
Strong214356 <tidiandelage1@gmail.com>
2022-09-18 20:15:12 +0200

    Setup of the logging system

--- Library/PlsEngine/Log.h
+++ Library/PlsEngine/Log.h
@@ -6,16 +6,20 @@
 #define PLSENGINE_LOG_H
 
 //core logger macros
+#define CORE_TRACE(...) PlsEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define CORE_INFO(...) PlsEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define CORE_WARN(...) PlsEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define CORE_ERROR(...) PlsEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CORE_TRACE(...) PlsEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CORE_FATAL(...) PlsEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
 
 //client logger macros
+#define CLIENT_TRACE(...) PlsEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define CLIENT_INFO(...) PlsEngine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define CLIENT_WARN(...) PlsEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define CLIENT_ERROR(...) PlsEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CLIENT_TRACE(...) PlsEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CLIENT_FATAL(...) PlsEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
 
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
